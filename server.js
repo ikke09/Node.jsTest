b69ea8cf3bc65f5d08926b789615ced24eee7758
Christian Bechtluft <christian.bechtluft@games-broduction.com>
2016-06-12 16:14:13 +0200

    + server.js with express + index.html angelegt + Ordnerstruktur angelegt -> Server liefert index.html datei aus. Diese beinhaltet ein canvas mit easeljs und zeichnet einen Kreis

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname+"/html"));
+
+app.get("/", function(req, res){
+	console.log("Request incomming");
+	res.sendFile(__dirname+"/html/index.html");
+	console.log("file send");
+})
+
+app.listen(3000);
+console.log("Server started");
