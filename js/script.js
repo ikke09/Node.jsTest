207c9157507c48444911cde4eddbd73b00e83402
Christian Bechtluft <christian.bechtluft@games-broduction.com>
2016-06-12 16:44:06 +0200

    + Versuch mit requierjs js-Dateien nachzuladen + index.html angepasst + alle installierten node module nicht mehr ignorieren

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-var canvas = document.getElementById("canvas");
-//Create a stage by getting a reference to the canvas
-stage = new createjs.Stage("canvas");
-//Create a Shape DisplayObject.
-circle = new createjs.Shape();
-circle.graphics.beginFill("red").drawCircle(0, 0, 40);
-//Set position of Shape instance.
-circle.x = circle.y = 50;
-//Add Shape instance to stage display list.
-stage.addChild(circle);
-//Update stage will render next frame
-stage.update();
+requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: '../app/lib',
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+        app: '../'
+    }
+});
+
+// Start the main app logic.
+requirejs(['easljs', 'app/main'],
+function   (main) {
+	main.draw();
+});
